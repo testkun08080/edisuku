a6866647dcd1d614bba2565c32bab5f68cae905c
testkun <154046906+testkun08080@users.noreply.github.com>
2026-06-16 19:27:22 +0700

    add site url

--- apps/web/env.d.ts
+++ apps/web/env.d.ts
@@ -3,6 +3,7 @@ declare namespace Cloudflare {
     ASSETS: Fetcher;
     INTERNAL_API_KEY: string;
     API_UPSTREAM_URL?: string;
+    PUBLIC_ENV__SITE_URL?: string;
     API?: { fetch(req: Request): Promise<Response> };
   }
 }
